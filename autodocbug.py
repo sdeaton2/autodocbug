bea558cac1efac1d44b76f2c70d2fe6d2099146f
Steven Deaton <sdeaton2@gmail.com>
2013-05-15 05:49:17 +0000

    - Added basic auth to LP and also added debug capability.

--- autodocbug.py
+++ autodocbug.py
@@ -2,9 +2,25 @@
 
 import os
 import sys
-import launchpadlib
+
+# For debugging output, both of these lines are needed:
+#import httplib2
+#httplib2.debuglevel = 1
+
+#import launchpadlib
+# ..not sure why, but this print only works if you import as above.
+#print launchpadlib.__version__
+from launchpadlib.launchpad import Launchpad
+
+# vars for later use
+homedir = os.getenv("HOME")
+#print "User home directory: %s" % homedir
+cachedir = homedir + "/.launchpadlib/cache/"
+#print "Cache directory: %s" % cachedir
 
 # connect to launchpad (auth, etc.)
+launchpad = Launchpad.login_with('autodocbug', 'staging')
+#launchpad = Launchpad.login_with('autodocbug', 'production')
 
 # get list of bugs on page
 
