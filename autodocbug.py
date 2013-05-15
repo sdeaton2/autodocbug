f65daf5ea6232740dfa40cb38573f25939b70dd0
Steven Deaton <sdeaton2@gmail.com>
2013-05-15 05:13:33 +0000

    - Initial commit.

---
+++ autodocbug.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import launchpadlib
+
+# connect to launchpad (auth, etc.)
+
+# get list of bugs on page
+
+# loop through list of bugs, checking for #DocImpact flag
+
+# operate on #DocImpact flag, creating new doc bug.
